bbc7778220040dc7e1d47182e426caf423be155e
Taffie Theuri <taffietheuri4@gmail.com>
2022-11-18 23:53:39 +0300

    Add about section.Will fix bug

---
+++ script.js
@@ -0,0 +1,12 @@
+/*var sectionlinks = document.getElementsByClassName("section-links");
+var sectioncontent = document.getElementsByClassName("section-contents");
+ function opentab(_sectionName){
+    for(sectionlinks of sectionlinks){
+        sectionlinks.classList.remove("active-link");
+    }
+    for(sectioncontent of sectioncontent){
+        sectioncontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById("_sectionName").classList.add("active-tab")
+ }*/
